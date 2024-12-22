6eb66230e88878b0fe965f04cb8adef525b138c7
medericc <bleublanctour1@>
2024-12-22 21:28:52 +0100

    ee

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-8 ">
-      <h1 className="text-4xl font-bold text-center mb-8">All Star LFB</h1>
+      <h1 className="text-4xl font-bold text-center mb-8">All Star LF2</h1>
       <div className="flex flex-wrap -mx-4">
         {renderTable("Conférence Est", eastPlayers, "east")}
         {renderTable("Conférence Ouest", westPlayers, "west")}
